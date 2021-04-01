0cf5d22f63f40fb2dcbb2a606f89a95fd974b0d5
mostafa <mostafo7102000@gmail.com>
2021-04-01 18:07:13 +0200

    finshed project react chipping

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "./HeroSection.css";
 import { Button } from "./Navbar/Button";
-import img from "../img/delivery.png";
+import {Link} from 'react-router-dom'
+import img from "../img/main_files/fast delivery service.png";
 function HeroSection() {
   return (
     <div className="hero-section">
       <div className="container">
-          <div className="banner display-table"> 
-          <div className="head-info table-cell text-center"> 
-        <div className="row">
-          <div className="col-md-6">
-              <div className="info">
-                <h1>Shapping Awaits</h1>
+           <div className="row">
+          <div className="col-md-6 ">
+            <div className="info">
+                <h3 className="title-text">Hey</h3>
+                <h1 className="title-text text-uppercase">Shapping Awaits</h1>
                 <p>whats are you wating for</p>
-                <Button
+                <Button 
+                to="/about"
                 className="btns"
                 buttonStyle="btn--outline"
                 buttonSize="btn--large"
                 >
-                Get Start
+                  <Link style={{"color": "#425b73" , "text-decoration":" none"}} to="/shapping">
+                  Get Start
+                  </Link> 
                 </Button>
-              </div>
+                </div>
           </div>
           <div className="col-md-6">
               <div className="cont-img">
@@ -30,8 +33,6 @@ function HeroSection() {
         </div>
         </div>
       </div>
-      </div>
-    </div>
   );
 }
 
