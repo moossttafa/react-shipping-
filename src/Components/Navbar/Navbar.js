cf988dd3b83d979a498f381c5fc6d737daaae88f
moossttafa <mostafo7102000@gmail.com>
2022-03-24 22:01:25 +0200

    edit style

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -34,56 +34,58 @@ const Navbar = () => {
     }
 window.addEventListener("scroll" , changeBackground);
     return (
-               <>
-            <div className={nav ? 'navbar active' : 'navbar'}>
-                <div className="navbar-container container">
-                  <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-                      <FaShippingFast className="navbar-icon " />
-                    FE ELENGAZ
-                  </Link>
-                  <div className='menu-icon' onClick={handleClick}>
-                      {click ? <FaTimes className="icons-nav"/> : <FaBars className="icons-nav"/>}
-                  </div>
-                  <ul className={click ? 'nav-menu active' : 'nav-menu'} >
-                    <li className="nav-item ">
-                        <Link to="/" className="nav-links"  onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-links"  onClick={closeMobileMenu}>
-                            About
-                        </Link>
-                    </li>
-                    <li className ="nav-item">
-                        <Link to='/contactus' className="nav-links"  onClick={closeMobileMenu}>
-                          Contact US 
-                        </Link>
-                    </li>
-                    <li className ="nav-item">
-                        <Link to='/shapping' className="nav-links"  onClick={closeMobileMenu}>
-                           Shapping
-                        </Link>
-                    </li>
-                    <li className ="nav-btn">
-                        {button ? (
-                            <Link to="/register" className="btn-link">
-                                <Button  buttonStyle="btn--outline">   SignUp
-                                </Button>
-                            </Link>
-                        ): (
-                            <Link to="/register" className="btn-link"  onClick={closeMobileMenu}>
-                                <Button buttonStyle="btn--outline" 
-                                        buttonSize="btn--mobile">
-                                SignUp
-                                </Button>
-                            </Link>
-                        )}
-                    </li>
-                  </ul>
-                </div>
-            </div>   
-        </>   
+			<>
+				<div className={nav ? 'navbar active' : 'navbar'}>
+						<div className="navbar-container container">
+							<Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
+									<FaShippingFast className="navbar-icon " />
+								FE ELENGAZ
+							</Link>
+							<div className='menu-icon' onClick={handleClick}>
+									{click ? <FaTimes className="icons-nav"/> : <FaBars className="icons-nav"/>}
+							</div>
+							<div className={click ? 'menu active' : 'menu'}>
+								<ul className='nav-menu' >
+										<li className="nav-item ">
+												<Link to="/" className="nav-links"  onClick={closeMobileMenu}>
+														Home
+												</Link>
+										</li>
+										<li className="nav-item">
+												<Link to="/about" className="nav-links"  onClick={closeMobileMenu}>
+														About
+												</Link>
+										</li>
+										<li className ="nav-item">
+												<Link to='/contactus' className="nav-links"  onClick={closeMobileMenu}>
+												Contact US 
+												</Link>
+										</li>
+										<li className ="nav-item">
+												<Link to='/shapping' className="nav-links"  onClick={closeMobileMenu}>
+												Shapping
+												</Link>
+										</li>
+										<li className ="nav-btn">
+												{button ? (
+														<Link to="/register" className="btn-link">
+																<Button  buttonStyle="btn--outline">   SignUp
+																</Button>
+														</Link>
+												): (
+														<Link to="/register" className="btn-link"  onClick={closeMobileMenu}>
+																<Button buttonStyle="btn--outline" 
+																				buttonSize="btn--mobile">
+																SignUp
+																</Button>
+														</Link>
+												)}
+										</li>
+								</ul>
+							</div>
+						</div>
+				</div>   
+		</>   
      )
 }
 
